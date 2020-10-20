bad38c157757fdb4df31be7aff8129656ca461c6
daiibee <deborah.gmuer@students.fhnw.ch>
2020-10-20 16:51:57 +0200

    created and updated project

---
+++ label_ui/Assets/display.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class display : MonoBehaviour
+{
+
+    public string myString;
+    public Text myText;
+    public float fadeTime;
+    public bool displayInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText = GameObject.Find("Text").GetComponent<Text>();
+        myText.color = Color.clear;
+        Cursor.visible = false;
+        Screen.lockCursor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        FadeText();
+
+     /*   if (Input.GetKeyDown(KeyCode.Escape))
+
+            {
+                Screen.lockCursor = false;
+            }
+     */
+    }
+
+    void OnMouseOver()
+    {
+        displayInfo = true;
+    }
+
+    void OnMouseExit()
+    {
+        displayInfo = false;
+
+    }
+
+    void FadeText()
+    {
+
+        if(displayInfo)
+        {
+            myText.text = myString;
+            myText.color = Color.Lerp(myText.color, Color.white, fadeTime * Time.deltaTime);
+        }
+
+        else
+        {
+            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+    }
+}
